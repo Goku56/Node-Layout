fbe0a232792c2cffbd2626788a480dbf1ec28fe2
Gokul <gokulmodi369@gmail.com>
2024-05-24 01:34:28 +0530

    add dockerfile and .dockerignore

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config();
 
 const { PORT } = process.env;
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 export const Config = {
   PORT,
